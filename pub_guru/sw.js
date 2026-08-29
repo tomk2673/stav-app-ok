50ce0155ee93b433ad888ab595ad15637139ec03
tomk2673 <80183726+tomk2673@users.noreply.github.com>
2026-08-29 13:15:02 +0200

    chore: cache authenticated Pub Guru backend modules

--- pub_guru/sw.js
+++ pub_guru/sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'pub-guru-shell-v2';
+const CACHE = 'pub-guru-shell-v3';
 const APP_SHELL = [
   './', './start.html', './index.html', './closings.html', './styles.css', './app.js', './closings.js',
-  './backend.js', './app-config.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'
+  './backend.js', './invoice-backend.js', './app-config.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'
 ];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));

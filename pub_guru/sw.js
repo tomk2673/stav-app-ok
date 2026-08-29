9bdf897e658ac6f19ef37d1c47d319fa2194f6ea
tomk2673 <80183726+tomk2673@users.noreply.github.com>
2026-08-29 12:52:34 +0200

    fix: update Pub Guru offline shell and auth entry

--- pub_guru/sw.js
+++ pub_guru/sw.js
@@ -1,5 +1,8 @@
-const CACHE = 'stav-shell-v1';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+const CACHE = 'pub-guru-shell-v2';
+const APP_SHELL = [
+  './', './start.html', './index.html', './closings.html', './styles.css', './app.js', './closings.js',
+  './backend.js', './app-config.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'
+];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', event => {
@@ -9,5 +12,5 @@ self.addEventListener('fetch', event => {
       const copy = res.clone(); caches.open(CACHE).then(c => c.put(event.request, copy));
     }
     return res;
-  }).catch(() => caches.match('./index.html'))));
+  }).catch(() => caches.match('./start.html'))));
 });
